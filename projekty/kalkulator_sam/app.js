064c36ccb8e85d63b0003f494a6702d8a99afb03
Osin99 <osinnn1999@gmail.com>
2023-06-24 12:07:34 +0200

    test

--- projekty/kalkulator_sam/app.js
+++ projekty/kalkulator_sam/app.js
@@ -1,9 +1,87 @@
 const currentNumber = document.querySelector(".currentNumber")
 const previousNumber = document.querySelector(".previosuNumber p")
 const mathSign = document.querySelector(".mathSign")
-const numbersButton = document.querySelector(".number")
-const operatorsButtons = document.querySelector(".operator")
+const numbersButtons = document.querySelectorAll(".number")
+const operatorsButtons = document.querySelectorAll(".operator")
 const equalsButton = document.querySelector(".equals")
 const clearButton = document.querySelector(".clear")
 const calculatorHsitory = document.querySelector(".history")
 const historyBtn = document.querySelector(".history-btn")
+
+
+
+
+let result =''
+
+
+
+
+function displayNumbers(){
+    if(this.textContent === '.' && currentNumber.innerHTML.includes('.')) return;
+    if(this.textContent ==='.' && currentNumber.innerHTML === '')return currentNumber.innerHTML = '.0'
+    currentNumber.innerHTML += this.textContent
+    
+}
+
+function operate(){
+    if(currentNumber.innerHTML ==='' && this.textContent ==='-'){
+        currentNumber.innerHTML = '-'
+        return
+    }
+    else if(currentNumber.innerHTML ===''){
+        return
+    }
+    if(mathSign.innerHTML !==''){
+        showResult();
+    }
+    previousNumber.innerHTML = currentNumber.innerHTML
+    mathSign.innerHTML = this.textContent
+    currentNumber.innerHTML = ''
+}
+
+function showResult(){
+
+}
+
+function clearScreen(){
+
+}
+
+function clearHistory(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//nasłuchiwanie
+
+operatorsButtons.forEach((button)=>{
+    button.addEventListener('click', operate)
+})
+
+equalsButton.addEventListener('click', showResult)
+
+clearButton.addEventListener('click', clearScreen)
+
+numbersButtons.forEach((button)=>{
+    button.addEventListener('click', displayNumbers)
+
+})
+
+historyBtn.addEventListener('click',clearHistory)
